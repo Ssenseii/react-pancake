b03f7ac4a19979ecb1126b5878094fa699f5e788
Simon <89594899+Ssenseii@users.noreply.github.com>
2024-11-07 17:45:10 +0100

    better color, will fix later

--- src/components/dashboard/DB_Ecommerce.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-declare const DB_Ecommerce: React.FC;
-export default DB_Ecommerce;
--- src/pages/inventory_sys/InventoryDashboard.jsx
+++ src/pages/inventory_sys/InventoryDashboard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import PageTitle from "../../components/global/PageTitle";
 
 const InventoryDashboard = () => {
-  return (
-    <div>Inventory Dashboard</div>
-  )
-}
+	return (
+		<div className="inventory__dashboard">
+			<div className="row">
+				<PageTitle
+					pageTitle="Pancake: Inventory Management System"
+					pageDescription="Unlock Smarter Inventory Management: Real-time Insights, Seamless Control."
+				></PageTitle>
+			</div>
+			<br />
+			<div className="row">
+				<div className="card col-xs-12 col-md-6 col-lg-2">
+					<h3>Total Inventory Value</h3>
+					<p>24.514 MAD</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default InventoryDashboard;
